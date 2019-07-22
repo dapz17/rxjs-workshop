d35a9657530f1351b0bf9a2dd994259dd23d12d8
dpoveda <dpoveda@mcclatchyservices.com>
2019-07-21 23:04:22 -0500

    JS - Streaming and Throttling refresh event

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ var closeButton1 = document.querySelector('.close1');
 var closeButton2 = document.querySelector('.close2');
 var closeButton3 = document.querySelector('.close3');
 
-var refreshClicks = Rx.Observable.fromEvent(refreshButton, 'click');
+var refreshClickStream = Rx.Observable.fromEvent(refreshButton, 'click').throttleTime(1000);
 var close1Clicks = Rx.Observable.fromEvent(closeButton1, 'click');
 var close2Clicks = Rx.Observable.fromEvent(closeButton2, 'click');
 var close3Clicks = Rx.Observable.fromEvent(closeButton3, 'click');
 
-refreshClicks.subscribe(() => console.log('Refresh'));
+refreshClickStream.subscribe(() => console.log('Refresh'));
 close1Clicks.subscribe(() => console.log('click1'));
 close2Clicks.subscribe(() => console.log('click2'));
 close3Clicks.subscribe(() => console.log('click3'));

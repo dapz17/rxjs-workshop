5e4853731d074d650e2519a799078b0d0879e42c
dpoveda <dpoveda@mcclatchyservices.com>
2019-07-21 22:51:28 -0500

    JS - Taking the Click Buttons Events with RxJS

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var closeButton1 = document.querySelector('.close1');
 var closeButton2 = document.querySelector('.close2');
 var closeButton3 = document.querySelector('.close3');
 
-refreshButton.addEventListener("click", (e) => console.log('refresh', e.target));
-closeButton1.addEventListener("click", (e) => console.log('close1', e.target));
-closeButton3.addEventListener("click", (e) => console.log('close2', e.target));
-closeButton3.addEventListener("click", (e) => console.log('close3', e.target));
+var refreshClicks = Rx.Observable.fromEvent(refreshButton, 'click');
+var close1Clicks = Rx.Observable.fromEvent(closeButton1, 'click');
+var close2Clicks = Rx.Observable.fromEvent(closeButton2, 'click');
+var close3Clicks = Rx.Observable.fromEvent(closeButton3, 'click');

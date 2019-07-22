a501036e976b6e964fd4af737be1116eeeb7590f
dpoveda <dpoveda@mcclatchyservices.com>
2019-07-21 22:56:22 -0500

    JS - Subscribing the events with RxJS

--- script.js
+++ script.js
@@ -7,3 +7,8 @@ var refreshClicks = Rx.Observable.fromEvent(refreshButton, 'click');
 var close1Clicks = Rx.Observable.fromEvent(closeButton1, 'click');
 var close2Clicks = Rx.Observable.fromEvent(closeButton2, 'click');
 var close3Clicks = Rx.Observable.fromEvent(closeButton3, 'click');
+
+refreshClicks.subscribe(() => console.log('Refresh'));
+close1Clicks.subscribe(() => console.log('click1'));
+close2Clicks.subscribe(() => console.log('click2'));
+close3Clicks.subscribe(() => console.log('click3'));
